5a1a5a3ffda728c4a40eff94f0ffdcae51194498
Gabriel Pereira <gabrielpereira160101@gmail.com>
2024-07-23 21:43:02 -0300

    Feito a conexao com o banco de dados da classe medicos e pacientes

--- src/main/java/med/voll/api/paciente/Paciente.java
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -1,41 +1,36 @@
-package med.voll.api.medico;
+package med.voll.api.paciente;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import med.voll.api.endereco.DadosEndereco;
 import med.voll.api.endereco.Endereco;
 
-@Table(name="medicos")
-@Entity(name="Medico")
+@Table(name="pacientes")
+@Entity(name="paciente")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of="id")
-public class Medico {
+public class Paciente {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
-    private String crm;
     private String telefone;
-
-    @Enumerated(EnumType.STRING)
-    private Especialidade especialidade;
-
+    private String cpf;
     @Embedded
     private Endereco endereco;
 
-    //Construtor criado pra converter o dado do DTO
-    public Medico(DadosCadastroMedico dados){
+    public Paciente(DadosCadastroPacientes dados){
         this.nome=dados.nome();
         this.email=dados.email();
-        this.crm=dados.crm();
         this.telefone=dados.telefone();
-        this.especialidade=dados.especialidade();
+        this.cpf=dados.cpf();
         this.endereco=new Endereco(dados.endereco());
     }
-
 }
